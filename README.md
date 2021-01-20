9dac23c925d318e3cfcaa10d59fba86898b4482b
mirsella <mirsella@protonmail.com>
2021-01-20 13:32:05 +0100

    gacp Wed Jan 20 01:32:05 PM CET 2021

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 thanks @benawad for the fun site and project. https://github.com/benawad/stripcode
 you need to put a github personal acess token in .env, get one at https://github.com/settings/tokens
+
+this branch was for indexing all the correct answer, and after with another account selecting them, that how i got 492,190 points.
+i still got the js file to get all the answer, but not the one to complete them because my dumbass git reset --hard it (lost multiple hour of coding), through it's relatively easy to rewrite if you want. 
---
+++ getAnswers.js
@@ -0,0 +1,37 @@
+
+const fs = require('fs')
+const db = require('monk')('localhost/stripcode');
+const answers = db.get('answers');
+const puppeteer = require('puppeteer');
+
+const userDataDir = process.argv[2] || 'PrivateChromeSessions';
+(async () => {
+  const browser = await puppeteer.launch({headless: false, defaultViewport: {width: 1280, height: 720}, userDataDir: userDataDir});
+  const page = await browser.newPage();
+  await page.goto('https://stripcode.dev/ranked', {waitUntil: 'networkidle0'})
+
+  while (true) {
+    fs.existsSync('stop') && process.exit()
+    console.log("---------------------------------------")
+    await page.waitForSelector('[class*=text-bblack]')
+    await page.waitForSelector('h1[class*=my-8]')
+
+    let repolist = await page.$$eval('[class*=text-bblack]', el => el.map(el => el.textContent))
+    let file = await page.$eval('h1[class*=my-8]', el => el.textContent)
+    console.log("file :", file)
+
+    await page.click('[class*=text-bblack]')
+    await page.waitForSelector('[phx-click="nextQuestion"]', {timeout: 3000})
+      .catch(async e => {
+        await page.click('[class*=text-bblack]')
+        await page.waitForTimeout(3000)
+      })
+    let rightrepo = await page.$eval('button[class*=bg-green-100] > span', el => el.textContent)
+    let content = await page.$eval('[id="main-code-block"]', el => el.textContent)
+    console.log(rightrepo, content.length)
+    // await answers.update({ file: file }, { $set: { content: content } })
+    await answers.insert({file: file, repo: rightrepo, content})
+    await page.click('[phx-click="nextQuestion"]')
+    await page.waitForTimeout(500)
+  }
+})();
